a25d6f4caa6476ae2d30a7d69a5b6d696a4c55ea
marek <marek-schuetze@mail.de>
2022-10-06 17:15:23 +0200

    Try fix github problems

--- backend/src/main/java/com/example/backend/repo/CheatSheetRepo.java
+++ backend/src/main/java/com/example/backend/repo/CheatSheetRepo.java
@@ -2,7 +2,6 @@ package com.example.backend.repo;
 
 
 import com.example.backend.model.CheatSheet;
-import lombok.AllArgsConstructor;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
