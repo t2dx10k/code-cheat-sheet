ffca46615b95e31225e8aef430091e04aa5f7a3f
marek <marek-schuetze@mail.de>
2022-10-06 17:02:21 +0200

    FE: Add CheatGallery.tsx, Add AddCommands.tsx

--- backend/src/main/java/com/example/backend/repo/CheatSheetRepo.java
+++ backend/src/main/java/com/example/backend/repo/CheatSheetRepo.java
@@ -6,10 +6,6 @@ import lombok.AllArgsConstructor;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 
 @Repository
 public interface CheatSheetRepo extends MongoRepository<CheatSheet, String> {
