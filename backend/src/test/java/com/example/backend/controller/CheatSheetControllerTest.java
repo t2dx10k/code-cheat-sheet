c82e80825284efc53bc686b3e391bffda0c596cf
mirkovobel <mirkovobel@gmail.com>
2022-10-10 17:01:12 +0200

    Deploy first try

---
+++ backend/src/test/java/com/example/backend/controller/CheatSheetControllerTest.java
@@ -0,0 +1,9 @@
+package com.example.backend.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class CheatSheetControllerTest {}
